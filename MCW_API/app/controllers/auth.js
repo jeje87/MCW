3ef08020af3e5e3b518f1156d76fbc6e752d2a35
jeje87 <jeromepiquet@free.fr>
2014-10-16 13:39:06 +0200

    1er commit avec webstorm

--- MCW_API/app/controllers/auth.js
+++ MCW_API/app/controllers/auth.js
@@ -4,29 +4,31 @@ var Membre = require('../models/membre');
 var logger = require('../utils/logger'); 
 
 passport.use(new BasicStrategy(
+
   function(username, password, callback) {
     Membre.findOne({ login: username }, function (err, membre) {
       if (err) { 
           logger.info('Erreur ' + err );  
-          return callback(err); 
-      };
+          return callback(err);
 
-      // No user found with that username
+      }
+        // No user found with that username
       if (!membre) { 
-          logger.info('Login inconnu : ' + membre.login );  
-          return callback(null, false); 
-      };
+          logger.info('Login inconnu : ' + username );
+          return callback(null, false);
 
-      // Make sure the password is correct
+      }
+        // Make sure the password is correct
       membre.verifyMdp(password, function(err, isMatch) {
-        if (err) { return callback(err); };
-
-        // Password did not match
-        if (!isMatch) { 
-            logger.info('Authentification KO pour ' + membre.login );  
-            return callback(null, false); 
-        };
-        // Success
+          if (err) {
+              return callback(err);
+          }
+          // Password did not match
+        if (!isMatch) {
+            logger.info('Authentification KO pour ' + membre.login );
+            return callback(null, false);
+        }
+          // Success
         logger.info('Authentification OK pour ' + membre.login );  
         return callback(null, membre);
       });
